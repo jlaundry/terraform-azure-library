20cf9b1d51e0396e934b561bb2e712fef0b0cc08
Jed Laundry <jlaundry@jlaundry.com>
2021-08-16 17:20:02 +1200

    add pri,sec shared key

--- log-analytics/outputs.tf
+++ log-analytics/outputs.tf
@@ -11,6 +11,14 @@ output "name" {
     value = azurerm_log_analytics_workspace.log.name
 }
 
+output "primary_shared_key" {
+    value = azurerm_log_analytics_workspace.log.primary_shared_key
+}
+
 output "resource_group_name" {
     value = local.resource_group_name
+}
+
+output "secondary_shared_key" {
+    value = azurerm_log_analytics_workspace.log.secondary_shared_key
 }
