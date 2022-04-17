bab925d7a5b39fa0f7056faa24c00b949675b921
Jed Laundry <jlaundry@jlaundry.com>
2022-04-17 13:42:54 +1200

    add storage_account_resource_id

--- static-website-cloudflare/outputs.tf
+++ static-website-cloudflare/outputs.tf
@@ -2,3 +2,7 @@
 output "primary_blob_connection_string" {
   value = azurerm_storage_account.public.primary_blob_connection_string
 }
+
+output "storage_account_resource_id" {
+  value = azurerm_storage_account.public.resource_id
+}
