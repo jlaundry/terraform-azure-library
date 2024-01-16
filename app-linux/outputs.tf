02da0cc23b2df5550c4c39f86345489204f54f33
Jed Laundry <jlaundry@jlaundry.com>
2024-01-17 12:50:31 +1300

    add log_analytics_workspace_id output

--- app-linux/outputs.tf
+++ app-linux/outputs.tf
@@ -26,3 +26,12 @@ output "app_service_password" {
 output "identity" {
     value = azurerm_linux_web_app.app.identity
 }
+
+output "log_analytics_workspace_id" {
+    value = azurerm_log_analytics_workspace.log.workspace_id
+}
+
+output "log_analytics_primary_shared_key" {
+    value     = azurerm_log_analytics_workspace.log.primary_shared_key
+    sensitive = true
+}
--- func-linux/outputs.tf
+++ func-linux/outputs.tf
@@ -6,3 +6,12 @@ output "identity" {
 output "function_app_name" {
     value = azurerm_linux_function_app.func.name
 }
+
+output "log_analytics_workspace_id" {
+    value = azurerm_log_analytics_workspace.log.workspace_id
+}
+
+output "log_analytics_primary_shared_key" {
+    value     = azurerm_log_analytics_workspace.log.primary_shared_key
+    sensitive = true
+}
